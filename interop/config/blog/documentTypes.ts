2ede35d74f3bb96ec163a14380a6d75792f76bf4
GustaveWPM <jgeorge@perso.be>
2023-11-18 10:18:23 +0100

    chore: minor changes

--- interop/config/blog/documentTypes.ts
+++ interop/config/blog/documentTypes.ts
@@ -9,12 +9,6 @@ import {
 } from '../../types/contentlayerConfigTweakers';
 import type { AtomicContentLayerDocumentConfig, DocumentsTypesMetadatas } from '../../types/hell/contentlayerConfig';
 
-const defineDocumentType = (def: () => DocumentTypeDef<string>) =>
-  ({
-    type: 'document',
-    def
-  }) as const;
-
 const documentsTypesMetadatas: DocumentsTypesMetadatas = {
   CategoryOnePost: {
     name: 'CategoryOnePost',
@@ -25,6 +19,12 @@ const documentsTypesMetadatas: DocumentsTypesMetadatas = {
     categoryFolder: 'category-two'
   }
 } as const;
+
+const defineDocumentType = (def: () => DocumentTypeDef<string>) =>
+  ({
+    type: 'document',
+    def
+  }) as const;
 
 export const documentTypes: DocumentType[] = Object.values(documentsTypesMetadatas).reduce(
   (acc, documentTypeMetadatas) => {
